36eab5038f536a0b3573d922b4ea425fb2e09365
Philip <philip@Philips-MacBook-Pro-2.local>
2020-03-11 16:21:35 -0700

    final commit

---
+++ sprint-two/src/components/Home.js
@@ -0,0 +1,86 @@
+import React from "react";
+import Hero from "./Hero";
+import Main from "./Main";
+import Comments from "./Comments";
+import SideVideo from "./SideVideo";
+import axios from "axios";
+
+// API KEY "api_key": "ffb22bc6-6976-404c-9cd6-c366bb70ee9b"
+
+class Home extends React.Component {
+  state = {
+    sideVideos: [],
+    main: [],
+    comments: [],
+    allVideos: []
+  };
+
+  componentDidMount() {
+    // Gettting sidevideo info
+    axios
+      .get(
+        `https://project-2-api.herokuapp.com/videos?api_key=ffb22bc6-6976-404c-9cd6-c366bb70ee9b`
+      )
+      .then(response => {
+        const allVideos = response.data;
+        // Getting main content info with hard coded id value (only to see info displayed)
+        axios
+          .get(
+            `https://project-2-api.herokuapp.com/videos/1af0jruup5gu?api_key=ffb22bc6-6976-404c-9cd6-c366bb70ee9b`
+          )
+          //Comparing  hard coded main video id with side video id and then changing the state
+          .then(response => {
+            let sideVideos = allVideos.filter(
+              video => video.id !== "1af0jruup5gu"
+            );
+            const main = response.data;
+            const comments = response.data.comments;
+            this.setState({ sideVideos, main: [main], comments, allVideos });
+          });
+      });
+  }
+
+  componentDidUpdate(prevProps) {
+    // Updating current state
+    if (prevProps.match !== this.props.match) {
+      axios
+        .get(
+          `https://project-2-api.herokuapp.com/videos/${this.props.match.params.id}?api_key=ffb22bc6-6976-404c-9cd6-c366bb70ee9b`
+        )
+        .then(response => {
+          const main = response.data;
+          const comments = response.data.comments;
+          let sideVideos = this.state.allVideos.filter(
+            video => video.id !== this.props.match.params.id
+          );
+          this.setState({ sideVideos, main: [main], comments });
+        });
+    }
+  }
+
+  render() {
+    if (this.state.main.length === 0) {
+      return (
+        <div className="loading__container">
+          <h1 className="loading"></h1>
+        </div>
+      );
+    }
+    // Return the updated state after comparing id's as well as populating the main content with the new image
+    return (
+      <div>
+        <Hero hero={this.state.main[0].image} />
+        <div className="all-components__main">
+          <div className="all-components__one">
+            <Main main={this.state.main} />
+            <Comments comments={this.state.comments} />
+          </div>
+          <div className="all-components__two">
+            <SideVideo sideVideos={this.state.sideVideos} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Home;
---
+++ sprint-two/src/components/Upload.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+function Upload() {
+  return (
+    <div className="upload">
+      <h1 className="upload__title">Upload Video</h1>
+      <div className="upload__big-container">
+        <div className="upload__video-container">
+          <h2 className="upload__subtitle">VIDEO THUMBNAIL</h2>
+          <div className="upload__video"></div>
+        </div>
+        <form className="upload__input-container">
+          <h5 className="upload__input-container--text">TITLE YOUR VIDEO</h5>
+          <textarea
+            className="upload__text-container upload__text-container--one"
+            name="name"
+            rows="10"
+            cols="50"
+            placeholder="Add a title to your video"
+          ></textarea>
+          <h5 className="upload__input-container--text">
+            ADD A VIDEO DESCRIPTION
+          </h5>
+          <textarea
+            className="upload__text-container upload__text-container--two"
+            name="comment"
+            rows="10"
+            cols="50"
+            placeholder="Add a description of your video"
+          ></textarea>
+        </form>
+      </div>
+      <div className="upload__button-container">
+        <button className="upload__button-container--publish">PUBLISH</button>
+        <button className="upload__button-container--cancel">CANCEL</button>
+      </div>
+    </div>
+  );
+}
+
+export default Upload;
